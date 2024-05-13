d549f12e2235e1c9502a7695f447de8be36e5be6
AnasGabbadi <anas0gabbadi@gmail.com>
2024-05-13 12:33:25 +0100

    initial commit

---
+++ utils/getPageStaticProps.js
@@ -0,0 +1,80 @@
+import { gql } from "@apollo/client";
+import client from "client";
+import { cleanAndtransformBlocks } from "./cleanAndtransformBlocks";
+import { mapMainMenuItems } from "./mapMainMenuItems";
+
+export const getPageStaticProps = async (context) => {
+console.log("CONTEXT:", context);
+const uri = context.params?.slug ? `/${context.params.slug.join("/")}/` : "/";
+const { data } = await client.query({
+    query: gql`
+    query PageQuery($uri: String!) {
+        nodeByUri(uri: $uri) {
+            ... on Page {
+                id
+                title   
+                blocks(postTemplate: false)
+                seo {
+                    title
+                    metaDesc
+                }
+            }
+            ... on Property {
+                id
+                title   
+                blocks(postTemplate: false)
+                seo {
+                    title
+                    metaDesc
+                }
+            }
+        }
+        acfOptionsMainMenu {
+        mainMenu {
+            callToActionButton {
+            destination {
+                ... on Page {
+                uri
+                }
+            }
+            label
+            }
+            menuItems {
+            menuItem {
+                destination {
+                ... on Page {
+                    uri
+                }
+                }
+                label
+            }
+            items {
+                destination {
+                ... on Page {
+                    uri
+                }
+                }
+                label
+            }
+            }
+        }
+        }
+    }   
+    `,
+    variables: {
+        uri,
+    }
+});
+const blocks = cleanAndtransformBlocks(data.nodeByUri.blocks);
+return {    
+    props: {
+        seo: data.nodeByUri.seo,
+    mainMenuItems: mapMainMenuItems(
+        data.acfOptionsMainMenu.mainMenu.menuItems
+    ),
+    callToActionLabel: data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
+    callToActionDestination: data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
+    blocks,
+    },
+};
+};
